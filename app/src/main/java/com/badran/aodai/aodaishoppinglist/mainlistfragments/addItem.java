cff09c77cc9b579dfaef8a9ffd8b70756372201a
user <razimograbi44@gmail.com>
2017-10-09 14:11:21 +0300

    aditem page

--- app/src/main/java/com/badran/aodai/aodaishoppinglist/mainlistfragments/addItem.java
+++ app/src/main/java/com/badran/aodai/aodaishoppinglist/mainlistfragments/addItem.java
@@ -3,11 +3,16 @@ package com.badran.aodai.aodaishoppinglist.mainlistfragments;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.badran.aodai.aodaishoppinglist.R;
+
+import java.util.ArrayList;
 
 public class addItem extends AppCompatActivity {
     private EditText ETname;
@@ -16,6 +21,9 @@ public class addItem extends AppCompatActivity {
     private EditText ETprice;
     private Button BTNSave;
     private ImageButton iBTNimage;
+    private ListView LWitem;
+    ArrayList<String> listItems;
+    ArrayAdapter<String> adapter;
 
 
 
@@ -29,5 +37,28 @@ public class addItem extends AppCompatActivity {
         ETprice = (EditText) findViewById(R.id.ETprice);
         BTNSave = (Button) findViewById(R.id.BTNSave);
         iBTNimage = (ImageButton) findViewById(R.id.iBTNimage);
+        LWitem = (ListView) findViewById(R.id.LWitem);
+        listItems = new ArrayList<String>();
+        listItems.add("First Item - added on Activity Create");
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listItems);
+        LWitem.setAdapter(adapter);
+        BTNSave.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                listItems.add(EditText.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+        LWitem.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position,
+                                    long id) {
+                Toast.makeText(addItem.this, "Clicked", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+    }
+}
     }
 }
