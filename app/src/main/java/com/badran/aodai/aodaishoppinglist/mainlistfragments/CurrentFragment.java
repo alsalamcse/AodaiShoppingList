c9d2882d2e37ad205e318c95e09da208864aaec3
user <razimograbi44@gmail.com>
2017-10-25 13:23:09 +0300

    current,history

--- app/src/main/java/com/badran/aodai/aodaishoppinglist/mainlistfragments/CurrentFragment.java
+++ app/src/main/java/com/badran/aodai/aodaishoppinglist/mainlistfragments/CurrentFragment.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.badran.aodai.aodaishoppinglist.R;
 
@@ -13,6 +18,12 @@ import com.badran.aodai.aodaishoppinglist.R;
  * A simple {@link Fragment} subclass.
  */
 public class CurrentFragment extends Fragment {
+    private TextView TVTotal;
+    private TextView TVTotalValue;
+    private TextView TVCount;
+    private TextView TVCountValue;
+    private ImageButton IMBSAVE;
+    private ListView LSVCurrent;
 
 
     public CurrentFragment() {
@@ -24,7 +35,19 @@ public class CurrentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_current, container, false);
+        View view= inflater.inflate(R.layout.fragment_current, container, false);
+        TVTotal=(TextView) view.findViewById(R.id.TVTotal);
+        TVTotalValue=(TextView) view.findViewById(R.id.TVTotalValue);
+        TVCount=(TextView) view.findViewById(R.id.TVCount);
+        TVCountValue=(TextView) view.findViewById(R.id.TVCountValue);
+        IMBSAVE=(ImageButton) view.findViewById(R.id.IMBSAVE);
+        LSVCurrent=(ListView) view.findViewById(R.id.LSTVCurrent);
+
+        String []ar={"stam1","stam2","stam3"};
+        //ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,)
+
+        //4.
+        return view;
     }
 
 }
--- app/src/main/java/com/badran/aodai/aodaishoppinglist/mainlistfragments/HistoryFragment.java
+++ app/src/main/java/com/badran/aodai/aodaishoppinglist/mainlistfragments/HistoryFragment.java
@@ -6,13 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.badran.aodai.aodaishoppinglist.R;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HistoryFragment extends Fragment {
+    private ListView LSTHistory;
 
 
     public HistoryFragment() {
@@ -24,7 +28,9 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        View view= inflater.inflate(R.layout.fragment_history, container, false);
+        LSTHistory=(ListView) view.findViewById(R.id.LSTHistory);
+        return view;
     }
 
 }
